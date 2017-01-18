92a58a20e9a36b2f8499edf23eedc1a4f955f803
Josh Tiras <tiras.j@gmail.com>
2017-01-18 11:27:45 -0800

    Fix dev requirements

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -2,6 +2,7 @@
 set -e -x
 
 for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install -r /io/requirements-dev.txt
     "${PYBIN}/python" /io/setup.py test
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
