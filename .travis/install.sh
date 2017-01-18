8d18cef2eb1444eb03dd93a4f31b0620412b6f2a
Josh Tiras <tiras.j@gmail.com>
2017-01-18 12:13:42 -0800

    Change travis build behavior

---
+++ .travis/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Currently not needed
+exit 0
--- travis/build-wheels.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e -x
-
-for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /io/requirements-dev.txt
-    "${PYBIN}/python" /io/setup.py test
-    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
-done
