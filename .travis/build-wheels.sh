68a519e02ec2a01fe1b04f6b297c59c043ea7bd2
Josh Tiras <tiras.j@gmail.com>
2017-01-24 20:50:24 -0800

    Temporarily remove tests

--- .travis/build-wheels.sh
+++ .travis/build-wheels.sh
@@ -5,7 +5,7 @@ set -e -x
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" install -r /io/requirements-dev.txt
-    "${PYBIN}/python" /io/setup.py test
+#    "${PYBIN}/python" /io/setup.py test
     "${PYBIN}/pip" wheel /io/ -w /io/wheelhouse/
 done
 
