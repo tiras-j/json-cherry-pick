532fa6e98c9c1f11f409e28e22a589c7b93b255f
Joshua Tiras <jot@yelp.com>
2017-01-24 18:48:53 -0800

    Try building with manylinux containers

---
+++ .travis/build-wheels.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e -x
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install -r /io/requirements-dev.txt
+    "${PYBIN}/pip" wheel /io/ -w /io/wheelhouse/
+done
+
+# Bundle - don't think we need this, keeping commented
+#for whl in wheelhouse/*.whl; do
+#    auditwheel repair "$whl" -w /io/wheelhouse/
+#done
+
+for PYBIN in /opt/python/*/bin/; do
+    ${PYBIN}/pip install json-cherry-pick --no-index -f /io/wheelhouse
+    (cd "$HOME"; ${PYBIN}/python setup.py test)
+done
