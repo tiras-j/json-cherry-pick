09e2015c2cb4556461fd4ffba870790e072a3e31
Josh Tiras <tiras.j@gmail.com>
2017-01-24 21:35:42 -0800

    Run auditwheel to fix platform tag

--- .travis/build-wheels.sh
+++ .travis/build-wheels.sh
@@ -5,13 +5,12 @@ set -e -x
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" install -r /io/requirements-dev.txt
-    "${PYBIN}/pip" wheel /io/ -w /io/wheelhouse/
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
-# Bundle - don't think we need this, keeping commented
-#for whl in wheelhouse/*.whl; do
-#    auditwheel repair "$whl" -w /io/wheelhouse/
-#done
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
+done
 
 for PYBIN in /opt/python/*/bin/; do
     "${PYBIN}/pip" install json-cherry-pick --no-index -f /io/wheelhouse
